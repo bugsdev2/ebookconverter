5f67ca196d679191ffffead347fbc2ae3da33095
BugsDev2 <msherlock246@gmail.com>
2023-03-01 22:13:50 +0530

    Added feature where the list of files is displayed on the screen.

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-
+const execSync = require('execSync');
+const form = require('formidable');
 
 
 const fileUpload = document.querySelector('#file-upload');
 const submit = document.querySelector('#submit-button');
 const ul = document.querySelector('#booklist');
+const convertButton = document.querySelector('#convert');
+
+const filesArr = [];
 
 submit.addEventListener('click', function(e){
     e.preventDefault();
-    for(let i=0;i<fileUpload.files.length;i++) {
+    if(ul.childElementCount == 0) {
+        for(let i=0;i<fileUpload.files.length;i++) {
+            filesArr.push(fileUpload.files[i].name);
+        }
+        filesArr.forEach(item => {
+            
+            const li = document.createElement('li');
+            li.textContent = item;
+            ul.appendChild(li);
         
+        })
+    }
+    
+
+    if(ul.childElementCount != 0) {
+        convertButton.setAttribute('style', "display: block;")
     }
 })
